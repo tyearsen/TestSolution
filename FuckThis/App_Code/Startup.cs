9dc57be4c031703be5f1b96fa197d85229d69d8a
Tye38 <tyea13@gmail.com>
2014-09-08 16:01:39 -0600

    Created new solution
    Its legit

---
+++ FuckThis/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FuckThis.Startup))]
+namespace FuckThis
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
